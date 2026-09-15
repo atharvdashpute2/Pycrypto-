f77df8a81412ce318d82c945dc8741a29ccbf2ab
atharvdashpute2 <atharvdashpute2@gmail.com>
2026-09-15 23:14:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def app_nav():
     pycrypto.config(menu=menu)
 
 def my_portfolio():
-    api_request = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=300&convert=USD&CMC_PRO_API_KEY=3889ce63-c733-403d-8b7d-c41a2438b4ea")
+    api_request = requests.get("Enter the API")
     api = json.loads(api_request.content)
 
     cursorObj.execute("SELECT * FROM coin")
